08a4d4850437d64c6fd26b6d168d0d0378b6e759
taxio <taxio.2029@gmail.com>
2018-04-30 17:11:21 +0900

    閾値を自動で調整

--- parser.py
+++ parser.py
@@ -12,15 +12,25 @@ if __name__ == '__main__':
     img_width = orign_img.shape[0]
     img_height = orign_img.shape[1]
 
-    thresh = 100
-    _, bin_img = cv2.threshold(gray_img, thresh, 255, cv2.THRESH_BINARY_INV)
+    bin_thresh = 100
+    _, bin_img = cv2.threshold(gray_img, bin_thresh, 255, cv2.THRESH_BINARY_INV)
 
-    # ここで欲しいのは
-    lines = cv2.HoughLines(bin_img, 1, np.pi/180.0, 1500, np.array([]), 0, 0)
+    # ここで欲しいのは14本の線なので，大体20~28本の線が見つかるまで閾値を調整し続ける
+    # TODO: 無限ループになる可能性あり
+    line_thresh = int(min(img_width, img_height)/2)
+    while True:
+        lines = cv2.HoughLines(bin_img, 1, np.pi / 180.0, line_thresh, np.array([]), 0, 0)
+        if lines is not None:
+            n_lines, _, _ = lines.shape
+            if n_lines < 20:
+                line_thresh += 10
+            elif n_lines > 28:
+                line_thresh -= 10
+            else:
+                print(n_lines)
+                break
 
-    a, b, c = lines.shape
-    print(a)
-    for i in range(a):
+    for i in range(n_lines):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
         a = math.cos(theta)
