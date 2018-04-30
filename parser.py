075dcd31b1e407a025cdc2900f4848eaf2681acb
taxio <taxio.2029@gmail.com>
2018-04-30 18:52:53 +0900

    各periodeで分割できるようにした

--- parser.py
+++ parser.py
@@ -43,8 +43,10 @@ if __name__ == '__main__':
         pt1 = (int(x0 + img_height* (-b)), int(y0 + img_width * (a)))
         pt2 = (int(x0 - img_height * (-b)), int(y0 - img_width * (a)))
         theta = int(theta*180/np.pi)
+        direction = 1   # 1:縦線 -1:横線
         if theta in range(89, 92):  # 横線(89~91度以内)
             dst_img = bin_img[pt1[1]-2:pt2[1]+2, 0:]
+            direction = -1
         elif theta in range(-1, 2): # 縦線(-1~1度以内)
             dst_img = bin_img[0:, pt1[0]-2:pt2[0]+2]
         else:
@@ -53,28 +55,62 @@ if __name__ == '__main__':
         cand = {
             "pt1": pt1,
             "pt2": pt2,
-            "avg": avg
+            "avg": avg,
+            "direction": direction
         }
         cands.append(cand)
 
     avg_thresh = 10
     while True:
         frames = list()
+        verticals = list()
+        sides = list()
         for cand in cands:
             if cand['avg'] > avg_thresh:
-                frames.append(cand)
-        if len(frames) == 14:
-            break
+                if cand['direction'] == 1:
+                    verticals.append(cand)
+                else:
+                    sides.append(cand)
 
-        if len(frames) < 14:
+        if (len(verticals)+len(sides)) == 14:
+            if len(verticals) == 8 and len(sides) == 6:
+                break
+            else:
+                print("frame cannot found")
+                exit(1)
+
+        if (len(verticals)+len(sides)) < 14:
             avg_thresh -= 1
         else:
             avg_thresh += 1
 
     print("found")
 
-    for frame in frames:
-        cv2.line(orign_img, frame['pt1'], frame['pt2'], (0, 0, 255), 2, cv2.LINE_AA)
+    # 縦線，横線ごとにソート
+    print("verticals")
+    verticals = sorted(verticals, key=lambda x: x['pt1'][0])
+    for v in verticals:
+        print(v['pt1'], v['pt2'])
+        cv2.line(orign_img, v['pt1'], v['pt2'], (0, 0, 255), 2, cv2.LINE_AA)
+
+    print("sides")
+    sides = sorted(sides, key=lambda x: x['pt1'][1])
+    for s in sides:
+        print(s['pt1'], s['pt2'])
+        cv2.line(orign_img, s['pt1'], s['pt2'], (255, 0, 0), 2, cv2.LINE_AA)
+
+    # 各組み合わせで表示
+    monday = list()
+    weeks = list()
+    for w in range(3):
+        days = list()
+        for i in range(6):
+            dst_img = orign_img[sides[w + 2]['pt1'][1]:sides[w + 3]['pt1'][1],
+                      verticals[i + 1]['pt1'][0]:verticals[i + 2]['pt1'][0]]
+            cv2.imshow("{}-{} period".format(w+1, i+1), dst_img)
+            cv2.waitKey(0)
+            days.append(dst_img)
+        weeks.append(days)
 
     cv2.imshow("test", orign_img)
     cv2.waitKey(0)
