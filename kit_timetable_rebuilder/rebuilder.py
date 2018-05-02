21c8534342b6e8aa4f409c1b51a9adc907da9a1b
taxio <taxio.2029@gmail.com>
2018-05-02 18:31:34 +0900

    全体のライブラリ構成を再構成

---
+++ kit_timetable_rebuilder/rebuilder.py
@@ -0,0 +1,10 @@
+"""
+ライブラリの中のメイン
+"""
+import kit_timetable_rebuilder as kitrb
+
+
+def rebuild(pdf_name: str):
+    # すっごい処理
+    # 再構築した画像 or pdfを返す
+    pass
--- kit_timetable_rebuilder/timetable.py
+++ kit_timetable_rebuilder/timetable.py
@@ -2,11 +2,11 @@ import numpy as np
 from logging import getLogger
 
 
-class Lesson(object):
+class TimeTable(object):
 
     def __init__(self, image: np.ndarray, logger=None):
         self.logger = logger or getLogger(__name__)
 
         self.image = image
 
-        # TODO: 課程の情報取得や複数コマかどうかの判定をする
+        # TODO: periodごとにパースして保持する処理
